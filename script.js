b13dadf524e4b23162332a3e4a6c73eaa450eec1
Ç♱ <linuxvhs@gmail.com>
2026-05-09 16:22:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-// Configurações do Contador de Água
-// Baseado no estudo "Making AI Less Thirsty" (2023)
-// Estimativa simbólica de consumo global em tempo real para fins de conscientização
+        let totalLiters = 0;
+        const display = document.getElementById('liters');
 
-let totalLiters = 0;
-const display = document.getElementById('liters');
-
-// A função roda a cada 1000 milissegundos (1 segundo)
-<script src="script.js"></script>
-
-// Log para confirmar que o script carregou corretamente
-console.log("Monitor de impacto ambiental ativado.");
+        setInterval(() => {
+            totalLiters += 0.05;
+            display.innerText = totalLiters.toFixed(2);
+        }, 1000);
