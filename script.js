c15e01355cc7976ba44b9f95ff6f2698888ab0b5
Ç♱ <linuxvhs@gmail.com>
2026-05-12 14:21:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-        let totalLiters = 0;
-        const display = document.getElementById('liters');
+document.addEventListener("DOMContentLoaded", () => {
+    let totalLiters = 0;
+    const display = document.getElementById('liters');
 
-        setInterval(() => {
-            totalLiters += 0.05;
-            display.innerText = totalLiters.toFixed(2);
-        }, 1000);
+    setInterval(() => {
+        totalLiters += 0.05;
+        display.innerText = totalLiters.toFixed(2);
+    }, 1000);
+});
